c10d9a49346250c3bf8035419249fa2bf8c197ad
Aidan Fingeret <aidan@Aidans-MacBook-Pro.local>
2020-12-18 13:11:07 -0500

    Edited bubbleSort method so it stops running when it is sorted.

--- Sorts.java
+++ Sorts.java
@@ -3,16 +3,19 @@ public class Sorts{
   public static void bubbleSort(int[] data){
     int leng = data.length;
     int holder = 0;
-    for(int i = 0; i < leng - 1; i++){
+    int counter = 1;
+    for(int i = 0; i < leng - 1 && counter > 0; i++){
+      counter = 0;
       for(int j = 1; j < (leng - i); j++){
         if(data[j-1] > data[j]){
           holder = data[j-1];
           data[j-1] = data[j];
           data[j] = holder;
+          counter++;
         }
       }
+      }
     }
-  }
 
   public static void selectionSort(int[] data){
     for(int i = 0; i < data.length - 1; i++){
